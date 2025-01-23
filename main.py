687200b8d8dfbef00c4201fb0fcab35a8f257728
Kaho Legradi <legradi.kaho@gmail.com>
2025-01-23 10:45:27 +0000

    final changes to the code: adding edit rating button and delete button

--- main.py
+++ main.py
@@ -54,24 +54,66 @@ with app.app_context():
 #     db.session.delete(book_to_delete)
 #     db.session.commit()
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def home():
-    return render_template('index.html')
+    with app.app_context():
+        result = db.session.execute(db.select(BookData).order_by(BookData.id))
+        all_books = result.scalars().all()
+    return render_template('index.html', books=all_books)
+
+@app.route('/delete')
+def delete():
+    book_id = request.args.get('book_id')
+    # DELETE A RECORD BY ID
+    book_to_delete = db.get_or_404(BookData, book_id)
+    # Alternative way to select the book to delete.
+    # book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
 
 
 @app.route("/add", methods=['GET','POST'])
 def add():
     if request.method == 'POST':
-        new_bood = BookData(
-            title=request.form['title'],
-            author=request.form['author'],
-            rating=request.form['rating']
-        )
-        db.session.add(new_bood)
-        db.session.commit()
-        return redirect(url_for('home'), id=new_bood.id)
+
+        with app.app_context():
+            # Check if the title already exists
+            existing_book = db.session.execute(
+                db.select(BookData).where(BookData.title == request.form['title'])
+            ).scalar()
+
+            if existing_book:
+                return "A book with this title already exists.", 400
+            else:
+                # getting id of the last data in the database
+                last_book = db.session.execute(db.select(BookData).order_by(BookData.id.desc())).scalar()
+                last_id = last_book.id if last_book else 0
+
+                new_book = BookData(
+                    id=last_id + 1, 
+                    title=request.form['title'],
+                    author=request.form['author'], 
+                    review=request.form['rating']
+                )
+
+                
+                db.session.add(new_book)
+                db.session.commit()
+                return redirect(url_for('home'))
     
     return render_template('add.html')
+
+@app.route('/edit/<book_id>', methods=['GET','POST'])
+def edit(book_id):
+    with app.app_context():
+        book = db.session.execute(db.select(BookData).where(BookData.id == book_id)).scalar()
+        if request.method == 'POST':
+            book.review = request.form['rating']
+            db.session.commit()
+            return redirect(url_for('home'))
+    return render_template('edit.html', book=book)
 
 
 if __name__ == "__main__":
